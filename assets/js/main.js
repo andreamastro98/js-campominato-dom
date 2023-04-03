d0dbb87a9e14d53c7482738cb874b1bb5643acae
andreamastro98 <andreamastrovito98@gmail.com>
2023-04-03 16:30:07 +0200

    esercizio completato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,13 @@
-//L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-//Ogni cella ha un numero progressivo, da 1 a 100.
-//Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-//Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-//Bonus
+//Copiamo la griglia fatta ieri nella nuova repo e aggiungiamo la logica del gioco (attenzione: non bisogna copiare tutta la cartella dell'esercizio ma solo l'index.html, e le cartelle js/ css/ con i relativi script e fogli di stile, per evitare problemi con l'inizializzazione di git).
+//Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell’array delle bombe non potranno esserci due numeri uguali.
+//In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+//La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+//Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha cliccato su una cella che non era una bomba.
+BONUS:
 //Aggiungere una select accanto al bottone di generazione, che fornisca una scelta tra tre diversi livelli di difficoltà:
-//- con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
-//- con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
-//- con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+//- difficoltà 1 ⇒ 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
+//- difficoltà 2 ⇒ 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
+//- difficoltà 3 ⇒ 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
 
 //trovo dove stampare l'elemento
@@ -18,28 +19,28 @@ document.getElementById(`play-button`).addEventListener(`click`, function(){
     var select = document.getElementById('select');
     var selectvalue = select.options[select.selectedIndex].value;
 
-
     griglia.innerHTML = ""
+    document.getElementById("result").style.display = "none";
     //Stampa della griglia in base alla scelta della difficoltá
     if (selectvalue == 1){
         creaGriglia( 100 )
         //imposto la griglia da 9x9
         myFunction_set(100)
-        //definisco box tot che mi servirá per le bombe     
+        //definisco il valore del calc dei box     
 
 
     } else if (selectvalue == 2){
         creaGriglia( 81 )
         //imposto la griglia da 9x9
         myFunction_set(81)
-        //definisco box tot che mi servirá per le bombe
+        //definisco il valore del calc dei box
         
         
     } else if (selectvalue == 3){
         creaGriglia( 49 )
         //imposto la griglia da 7x7
         myFunction_set(49)
-        //definisco box tot che mi servirá per le bombe
+        //definisco il valore del calc dei box
         
     }
 })
@@ -105,22 +106,41 @@ function creaGriglia( totbox ){
 
     console.log(arrayBombe)
 
+    let isGameOver = false
+    let score = 0
+
     for (let i = 1; i <= totbox; i++){
         //associamo la funzione all'elemento che vogliamo creare
         const divbox = creaElementoHtml ("div", "box", i)
 
         //rendo cliccabile l'elemento
         divbox.addEventListener(`click`, function(){
-            if(!arrayBombe.includes(i)){
-                //aggiungo la classe aliceblue
-                this.classList.toggle(`aliceblue`)
+            //controllo che il gioco non sia finito
+            if (!isGameOver){
+                //controllo se il box selezionato é nell'array e quindi é una bomba
+                if(!arrayBombe.includes(i)){
+                    //aggiungo la classe aliceblue
+                    this.classList.toggle(`aliceblue`)
+                    score++
+                } else {
+                    //aggiungo la classe red
+                    this.classList.add(`red`)
+                    //metto l'icona delle bombe al posto del numero
+                    this.innerHTML = `<i class="fa-solid fa-bomb fa-bounce" style="color: #000000;"></i>`
+                    //cambio il display del div RESULT
+                    document.getElementById("result").style.display = "block";
+                    //faccio uscire il messaggio nel div RESULT con lo score 
+                    document.getElementById("result").innerHTML = `Hai perso :( <br> Il tuo risultato é ${score}pt. <br> Clicca sul pulsante gioca o un qualsiasi quadrato per iniziare un'altra partita.`;
+                    //dico che il game é finito
+                    isGameOver = true
+                }       
             } else {
-                this.classList.add(`red`)
-            }            
+                griglia.innerHTML = ""
+                document.getElementById("result").style.display = "none";
+            }                 
         })
         griglia.append( divbox )
     }
-
 }
 
 function numRandom( min, max ){
